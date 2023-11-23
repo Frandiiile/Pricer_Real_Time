7c1ead09c96c80f5dc729cebbc06c2146d81171a
Aimane Frandile <95171284+Frandiiile@users.noreply.github.com>
2023-11-23 18:58:59 +0100

    Working scripts

--- Black_Scholes.py
+++ Black_Scholes.py
@@ -13,6 +13,4 @@ def BS_PUT(S, K, T, r, sigma):
     d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
     d2 = d1 - sigma* np.sqrt(T)
     return K*np.exp(-r*T)*N(-d2) - S*N(-d1)
-print(BS_CALL(100, 100, 1, 0.05, 0.25))
-print(BS_PUT(100, 100, 1, 0.05, 0.25))
 
